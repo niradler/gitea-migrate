d1c53097b6c6be3e80f888cc0d9a84697447ae32
maxgallup <m.gallup@student.vu.nl>
2022-04-15 00:44:31 +0200

    using reqwest::blocking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+
+
+use reqwest;
+
+fn main() -> Result<(), reqwest::Error> {
+
+
+    
+    let body = reqwest::blocking::get("https://www.rust-lang.org")?
+    .text()?;
+
+    println!("body = {:?}", body);
+
+
+    Ok(())
 }
