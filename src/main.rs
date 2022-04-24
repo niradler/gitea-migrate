77171ed44a11b660ab3e16a8bb5e1595b44d36d7
maxgallup <m.gallup@student.vu.nl>
2022-04-24 14:29:32 +0200

    improved cli help

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+use std::path::PathBuf;
 
 use reqwest::{self, header::HeaderMap};
 use reqwest::header::*;
@@ -21,158 +21,192 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// Migrate only public repos (default)
+    /// (default) migrate only public repos
     #[clap(long)]
     public: bool,
 
-    /// Migrate only private repos
+    /// migrate only private repos
     #[clap(long)]
     private: bool,
 
-    /// Migrate only public and private repos
+    /// migrate only public and private repos
     #[clap(long)]
     both: bool,
 
-    /// Migrate all repos (including associated repos)
+    /// migrate all repos (including associated repos)
     #[clap(long)]
     all: bool,
 
+    /// base url of github source repo
+    #[clap(short, long, value_name = "URL")]
+    from_url: String,
+
+    /// base url of gitea destination repo
+    #[clap(short, long, value_name = "URL")]
+    to_url: String,
+
     
-    /// Base url of github source repo
-    #[clap(long)]
-    from: String,
-
-    /// Base url of gitea destination repo
-    #[clap(long)]
-    to: String,
-
-
-
-
-}
-
-
-
-#[derive(Debug)]
-struct Repo {
-    name: String,
-    visibility: String,
-    owner: String,
-}
-
-fn get_token(path: &str) -> String {
-
-    let inner = match fs::File::open(path) {
-        Ok(file) => file,
-        Err(e) => panic!("{:?}", e),
-    };
+    /// print useful information to stdout
+    #[clap(short, long)]
+    verbose: bool,
     
-    let mut reader = BufReader::new(inner);
-    let mut buf: String = String::new();
-    reader.read_line(&mut buf).expect("Unable to read line");
-
-    buf
+    /// (optional) uses a custom credentials file with:
+    /// github_user:github_token gitea_user:gitea_password
+    #[clap(short, long, parse(from_os_str), value_name = "FILE")]
+    creds: Option<PathBuf>,
 }
 
-fn github_headers() -> HeaderMap {
 
 
-    let mut headers = HeaderMap::new();
-    let username = "maxgallup";
-    let password = get_token("token-github.txt");
+// #[derive(Debug)]
+// struct Repo {
+//     name: String,
+//     visibility: String,
+//     owner: String,
+// }
+
+// fn get_token(path: &str) -> String {
+
+//     let inner = match fs::File::open(path) {
+//         Ok(file) => file,
+//         Err(e) => panic!("{:?}", e),
+//     };
     
-    let encoded_credentials = base64::encode(format!("{}:{}", username, password));
-    let basic_auth = format!("Basic {}", encoded_credentials);
+//     let mut reader = BufReader::new(inner);
+//     let mut buf: String = String::new();
+//     reader.read_line(&mut buf).expect("Unable to read line");
+
+//     buf
+// }
+
+// fn github_headers() -> HeaderMap {
+
+
+//     let mut headers = HeaderMap::new();
+//     let username = "maxgallup";
+//     let password = get_token("token-github.txt");
     
-    headers.insert(AUTHORIZATION, HeaderValue::from_str(&basic_auth).unwrap());
-    headers.insert(USER_AGENT, HeaderValue::from_static("api"));
-    headers.insert(ACCEPT, HeaderValue::from_static("application/vnd.github.v3+json"));
-    headers
-}
+//     let encoded_credentials = base64::encode(format!("{}:{}", username, password));
+//     let basic_auth = format!("Basic {}", encoded_credentials);
+    
+//     headers.insert(AUTHORIZATION, HeaderValue::from_str(&basic_auth).unwrap());
+//     headers.insert(USER_AGENT, HeaderValue::from_static("api"));
+//     headers.insert(ACCEPT, HeaderValue::from_static("application/vnd.github.v3+json"));
+//     headers
+// }
 
-fn gitea_headers() -> HeaderMap {
-    let mut headers = HeaderMap::new();
-    headers.insert(USER_AGENT, HeaderValue::from_static("api"));
-    headers.insert(ACCEPT, HeaderValue::from_static("application/json"));
-    headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
-    headers
-}
+// fn gitea_headers() -> HeaderMap {
+//     let mut headers = HeaderMap::new();
+//     headers.insert(USER_AGENT, HeaderValue::from_static("api"));
+//     headers.insert(ACCEPT, HeaderValue::from_static("application/json"));
+//     headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+//     headers
+// }
 
-fn trim_quotes(v: &serde_json::Value) -> String {
-    v.to_string().trim_matches('\"').to_string()
-}
+// fn trim_quotes(v: &serde_json::Value) -> String {
+//     v.to_string().trim_matches('\"').to_string()
+// }
 
-fn get_content(r: &Repo) -> HashMap<String, String> {
+// fn get_content(r: &Repo) -> HashMap<String, String> {
 
 
 
-    HashMap::new()
-}
+//     HashMap::new()
+// }
 
 
 // fn migrate(r: &Repo) {
 //     gitea_client.post("ads").json(get_content(&repo)).send()?;
 // }
 
+#[derive(Debug)]
+struct RepoFilter {
+    public: bool,
+    private: bool,
+    any_owner: bool,
+}
+
+#[derive(Debug)]
+struct UserData {
+    gh: String,
+    gh_user: String,
+    gh_pass: String,
+    gt: String,
+    gt_user: String,
+    gt_pass: String,
+}
+
+
 fn main() -> Result<(), reqwest::Error> {
 
     let args = Args::parse();
 
+    println!(">>> {:?}", args.creds);
+    println!(">>> {:?}", args.to_url);
+    println!(">>> {:?}", args.from_url);
 
-    println!("Hello {}!", args.public);
-    println!("Hello {}!", args.private);
-    println!("Hello {}!", args.both);
-    println!("Hello {}!", args.all);
-    
+    let filter = RepoFilter {
+        public: !args.private || args.both || args.all,
+        private: args.private || args.both || args.all,
+        any_owner: args.all,
+    };
 
+    let user = UserData {
+        gh: args.from_url,
+        gt: args.to_url,
+        gh_user: "asdf".to_string(),
+        gt_user: "asdf".to_string(),
+        gh_pass: "asdf".to_string(),
+        gt_pass: "asdf".to_string(),
+    };
 
-    let user = "maxgallup";
-    let github_base = "https://api.github.com/";
-    let gitea_base = "https://git.basingse.org/api/v1";
-    let all_repos = "https://api.github.com/user/repos?per_page=200";
-    let public_repos = format!("{}users/{}/repos", github_base, user);
+    // let user = "maxgallup";
+    // let github_base = "https://api.github.com/";
+    // let gitea_base = "https://git.basingse.org/api/v1";
+    // let all_repos = "https://api.github.com/user/repos?per_page=200";
+    // let public_repos = format!("{}users/{}/repos", github_base, user);
 
-    let mut gh_db : Vec<Repo> = Vec::new();
+    // let mut gh_db : Vec<Repo> = Vec::new();
   
     
-    let gh_client = reqwest::blocking::Client::builder()
-        .default_headers(github_headers())
-        .build()?;
+    // let gh_client = reqwest::blocking::Client::builder()
+    //     .default_headers(github_headers())
+    //     .build()?;
 
 
-    let resp = gh_client.get(public_repos).send()?;
+    // let resp = gh_client.get(public_repos).send()?;
 
 
-    // println!("resp.status = {:?}", resp.status());
-    // println!("resp.headers = {:?}", resp.headers());
-    // println!("resp.text = {:?}", resp.text()?);
-    let body : serde_json::Value = serde_json::from_str(&resp.text()?).unwrap();
+    // // println!("resp.status = {:?}", resp.status());
+    // // println!("resp.headers = {:?}", resp.headers());
+    // // println!("resp.text = {:?}", resp.text()?);
+    // let body : serde_json::Value = serde_json::from_str(&resp.text()?).unwrap();
 
-    for repo in body.as_array().unwrap() {
+    // for repo in body.as_array().unwrap() {
         
-        gh_db.push( Repo {
-            name: trim_quotes(&repo["name"]),
-            visibility: trim_quotes(&repo["visibility"]),
-            owner: trim_quotes(&repo["owner"]["login"]),
-        });
-    }
+    //     gh_db.push( Repo {
+    //         name: trim_quotes(&repo["name"]),
+    //         visibility: trim_quotes(&repo["visibility"]),
+    //         owner: trim_quotes(&repo["owner"]["login"]),
+    //     });
+    // }
 
 
 
 
-    let gitea_username = "maxgallup";
-    let gitea_password = get_token("gitea-token.txt");
-    let gitea_client = reqwest::blocking::Client::builder()
-        .default_headers(gitea_headers())
-        .build()?;
+    // let gitea_username = "maxgallup";
+    // let gitea_password = get_token("gitea-token.txt");
+    // let gitea_client = reqwest::blocking::Client::builder()
+    //     .default_headers(gitea_headers())
+    //     .build()?;
 
-    // gitea_client.
+    // // gitea_client.
 
 
-    for repo in gh_db {
-        // migrate(repo)
-        println!("{:?}", repo);
-    }
+    // for repo in gh_db {
+    //     // migrate(repo)
+    //     println!("{:?}", repo);
+    // }
 
     Ok(())
 }
